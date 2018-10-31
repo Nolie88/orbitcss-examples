f4295b3f174dc48f40200051373e7ea091c079ee
Rhys Hall <rhysnhall@gmail.com>
2018-10-31 18:17:17 +1100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# orbitcss-examples
-Example Pages for OrbitCSS
+# Orbitcss examples
+Example pages for [OrbitCSS](https://orbitcss.com)
+
+## Getting Started
+These examples demonstrate the out-of-the box features of OrbitCSS and can act as a quick starting point for your own future projects.
+
+View the examples live on the [OrbitCSS examples page](https://orbitcss.com/examples).
+
+## Documentation
+You can browse the OrbitCSS [online documentation here](https://orbitcss.com/documentation).
---
+++ src/signin.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/orbitcss@0.1.1/css/orbit.min.css">
+    <title>Sign in Example</title>
+  </head>
+  <body>
+    <section class="hero is-full has-bg-light">
+      <div class="hero__content">
+        <div class="container has-text-center">
+          <form class="grid has-centered is-vertical" method="post">
+            <div class="column is-one-third">
+              <h3>Company Logo</h3>
+            </div>
+            <div class="column is-one-third panel has-bg-white has-no-mobile-box-shadow">
+              <div class="column is-full has-no-padding-bottom">
+                <h4>Login</h4>
+              </div>
+              <div class="column is-full">
+                <input type="email" placeholder="Email address" required>
+              </div>
+              <div class="column is-full">
+                <input type="password" placeholder="Password" required>
+              </div>
+              <div class="column is-full grid has-no-padding">
+                <div class="column is-mobile-column is-shrink">
+                  <label for="remember-me">Remember me</label>
+                </div>
+                <div class="column is-mobile-column is-shrink is-end">
+                  <div class="is-switch is-rounded">
+                    <input type="checkbox" id="remember-me">
+                    <label for="remember-me"></label>
+                  </div>
+                </div>
+              </div>
+              <div class="column is-full">
+                <button type="submit" class="button is-primary is-full">Login</button>
+              </div>
+            </div>
+            <div class="column is-full">
+              <a href="/register">Register</a> | <a href="/forgot">Forgot password</a>
+            </div>
+            <div class="column is-full">
+              <p>&copy; Company Name 2018. All Rights Reserved.</p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
